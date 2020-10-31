80f80f814f35c877ea5ac24569a0ae9033c176c0
Dheshan Mohandass <dheshan.mail@gmail.com>
2020-10-31 19:29:14 +0530

    Modified ENVI usage
    Signed-off-by: Dheshan Mohandass <dheshan.mail@gmail.com>

--- b01_run.bat
+++ b01_run.bat
@@ -1,1 +1,1 @@
-python main.py --stride_size=32 --use_gpu=1 --out_dir="%cd%\output" --input_data="%cd%\data" --save_npy='True'
+python main.py --stride_size=16 --use_gpu=1 --out_dir="%cd%\output" --input_data="%cd%\data" --save_npy='True'
